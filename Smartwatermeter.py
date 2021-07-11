1392969d8f45de901e56e6c4b8b92e364c90094d
nishasetty <85960509+nishasetty@users.noreply.github.com>
2021-07-11 11:59:16 +0530

    Update Smartwatermeter.py

--- Smartwatermeter.py
+++ Smartwatermeter.py
@@ -190,7 +190,7 @@ def main():
 
     #data_file = "waterdata.json"
     data_file = args.json_data_file
-    fr = open(data_file, 'w')
+    fr = open(data_file, 'r')
     i = 1
     for line in fr:
         data = json.loads(line)
